7f9f9afaea2a5b654e53034e19b4a43d4f2b46b7
Julien Brun <brunj7@users.noreply.github.com>
2024-09-13 15:52:09 -0700

    add URL to dataset

--- data-dryad/global.R
+++ data-dryad/global.R
@@ -1,5 +1,5 @@
 library(shiny)
-library(rdryad) #DEPRECATED
+library(rdryad) #DEPRECATED!!
 library(tidyverse)
 
 # Filename locally
@@ -7,7 +7,7 @@ cached_file <- "data/river_data_test.csv"
 
 # Only download the data if it is not yet cached locally
 if (!file.exists(cached_file)) {
-  # get a dataset
+  # get a dataset https://datadryad.org/stash/dataset/doi:10.5061/dryad.jh9w0vtcg
   file_names <- dryad_download(dois="10.5061/dryad.f74fs")
   river_data <- read_csv(file_names$`10.5061/dryad.f74fs`[[1]])
   write_csv(river_data, cached_file)
