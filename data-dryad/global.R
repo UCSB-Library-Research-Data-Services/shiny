9a88cf88db32154e4996f839e5d776a51cb5aa24
Julien Brun <brunj7@users.noreply.github.com>
2024-10-16 16:31:08 -0700

    making it a function and handling Zenodo

--- data-dryad/global.R
+++ data-dryad/global.R
@@ -3,16 +3,27 @@ library(rdryad) #DEPRECATED, and no replacement yet https://github.com/ropensci/
 library(tidyverse)
 
 # Filename locally
-cached_file <- "data/river_data_test.csv"
+my_cached_file <- "data-dryad/data/river_data_test.csv"
+my_doi = "10.5061/dryad.f74fs"
+my_repo = "Dryad"
 
-# Only download the data if it is not yet cached locally
-if (!file.exists(cached_file)) {
-  # get a dataset https://datadryad.org/stash/dataset/doi:10.5061/dryad.jh9w0vtcg
-  file_names <- dryad_download(dois="10.5061/dryad.f74fs")
-  river_data <- read_csv(file_names$`10.5061/dryad.f74fs`[[1]])
-  write_csv(river_data, cached_file)
+
+
+data_cache_checker <- function(cached_file, doi, repo="Dryad"){
+  # Only download the data if it is not yet cached locally
+  if (file.exists(cached_file)) {
+    data <- read.csv(cached_file) 
+  } else {
+    # get a dataset https://datadryad.org/stash/dataset/doi:10.5061/dryad.jh9w0vtcg
+    file_names <- dryad_download(dois=doi)
+    repo_data <- read_csv(file_names$doi[[1]])
+    # Write the data
+    write_csv(repo_data, cached_file)
+    # Read the data
+    data <- read.csv(cached_file)
+  }
 }
   
   
 
-
+data_cache_checker(my_cached_file, my_doi, my_repo)
